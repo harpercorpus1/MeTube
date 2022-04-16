221bbebff14e77b06e1646ac0a5fcec510375624
Harper Corpus <57370769+harpercorpus1@users.noreply.github.com>
2022-04-16 01:26:19 -0400

    added mysql and landing page

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/standard.css"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="topnav">
+  <div class="header-container"><p>MeTube</p></div>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+<div>
+  <h2>Metube Landing page</h2>
+</div>
+
+
+<div class="home-thumb">
+
+</div>
+</body>
+</html>
